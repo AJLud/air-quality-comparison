4e50c92eba26628d0efad2e8050b2c646a5fd207
AJLud <a.ludlowww@gmail.com>
2022-04-12 19:26:32 +0100

    AQ: refactor api request to async await

--- src/requests/getLocationData.ts
+++ src/requests/getLocationData.ts
@@ -1,19 +1,11 @@
 import axios from 'axios';
 
-const getLocationData = (selectedCity: any, citySelection: any, setCitySelection: any) => {
+const getLocationData = async (selectedCity: any, setCitySelection: any) => {
   const endpoint = `https://docs.openaq.org/v2/latest?sort=desc&city=${selectedCity}&order_by=lastUpdated&dumpRaw=false`;
-
-  axios
-    .get(endpoint)
-    .then((response) => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore:next-line
-      // eslint-disable-next-line no-shadow
-      setCitySelection((citySelection) => [...citySelection, response.data.results]);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  const response = await axios.get(endpoint);
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore:next-line
+  setCitySelection((citySelection) => [...citySelection, response.data.results[0]]);
 };
 
 export default getLocationData;
