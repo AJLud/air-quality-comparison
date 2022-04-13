bcea93faa3554b1209557325f4d4e578e0920901
AJLud <a.ludlowww@gmail.com>
2022-04-13 17:31:39 +0100

    AQ: remove unnecessary import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
 import CardDisplay from './components/CardDisplay/CardDisplay';
 import SearchBar from './components/SearchBar/SearchBar';
 import GlobalStyle from './globalStyles';
